ab4655df0a69980a228e76815ea9484593d8168d
laphayen <laphayen@gmail.com>
2023-12-07 09:33:35 +0000

    Update timer.c

--- devices/timer.c
+++ devices/timer.c
@@ -130,6 +130,7 @@ static void
 timer_interrupt (struct intr_frame *args UNUSED) {
 	ticks++;
 	thread_tick ();
+	thread_awake (ticks);
 }
 
 /* Returns true if LOOPS iterations waits for more than one timer
