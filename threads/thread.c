0dc2d445ac686a1fce6d6404ada7f5ed3ad5892e
laphayen <laphayen@gmail.com>
2023-12-14 10:17:43 +0000

    Update thread.c

--- threads/thread.c
+++ threads/thread.c
@@ -656,3 +656,19 @@ int64_t
 get_next_tick_to_awake (void) {
 	return next_tick_to_awake;
 }
+
+/* Priority Scheduling */
+void 
+test_max_priority (void) {
+
+}
+
+/* Priority Scheduling */
+/* Compare the priority of the given threads. */
+bool
+cmp_priority (const struct list_elem *a, const struct list_elem *b, void *aux UNUSED) {
+	struct thread* thread_a = list_entry(a, struct thread, elem);
+	struct thread* thread_b = list_entry(b, struct thread, elem);
+
+	return thread_a->priority > thread_b->priority;
+}
