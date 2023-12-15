62aa8c56b93d2abb8c651b564001271d2caf7742
laphayen <laphayen@gmail.com>
2023-12-15 09:16:23 +0000

    Update thread.c

--- threads/thread.c
+++ threads/thread.c
@@ -216,7 +216,11 @@ thread_create (const char *name, int priority,
 	t->tf.eflags = FLAG_IF;
 
 	/* Add to run queue. */
+	// 쓰레드를 ready 상태로 만들어 주고
 	thread_unblock (t);
+
+	// 우선순위가 높은 쓰레드를 찾아서 스케줄링한다.
+	test_max_priority();
 
 	return tid;
 }
@@ -251,7 +255,11 @@ thread_unblock (struct thread *t) {
 
 	old_level = intr_disable ();
 	ASSERT (t->status == THREAD_BLOCKED);
-	list_push_back (&ready_list, &t->elem);
+	// thread를 ready_list에 넣을 때 우선순위로 정렬하기 위해서
+	// 미리 생성한 함수를 사용해서 삽입한다.
+	// list_push_back (&ready_list, &t->elem);
+	list_insert_ordered(&ready_list, &t->elem, test_max_priority, NULL);
+
 	t->status = THREAD_READY;
 	intr_set_level (old_level);
 }
@@ -314,9 +322,14 @@ thread_yield (void) {
 
 	old_level = intr_disable ();
 	if (curr != idle_thread)
-		list_push_back (&ready_list, &curr->elem);
+		list_insert_ordered(&ready_list, &curr->elem, cmp_priority, NULL);
 	
 	do_schedule (THREAD_READY);
+
+	// 쓰레드가 반환하게 되면 ready_list의 threads에서 가장 높은
+	// 우선순위의 쓰레드가 실행될 수 있도록 해야한다.
+
+
 	intr_set_level (old_level);
 }
 
@@ -324,6 +337,7 @@ thread_yield (void) {
 void
 thread_set_priority (int new_priority) {
 	thread_current ()->priority = new_priority;
+	test_max_priority ();
 }
 
 /* Returns the current thread's priority. */
@@ -550,6 +564,7 @@ do_schedule(int status) {
 	schedule ();
 }
 
+/* schedule에서 문제가 발생 */
 static void
 schedule (void) {
 	struct thread *curr = running_thread ();
@@ -658,9 +673,15 @@ get_next_tick_to_awake (void) {
 }
 
 /* Priority Scheduling */
+/* 현재 실행중인 스레드와 가장 높은 우선순위의 스레드의 우선순위를 비교해서 스케줄링한다. */
 void 
 test_max_priority (void) {
+	struct list_elem *highest_pri_elem = list_begin(&ready_list);
+	struct thread *highest_pri_thread = list_entry(highest_pri_elem, struct thread, elem);
 
+	if (thread_current()->priority < highest_pri_thread->priority) {
+		thread_yield ();
+	}
 }
 
 /* Priority Scheduling */
