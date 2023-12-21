5043f9b1bb9d9545b898bc2df985422f0d5ec44e
laphayen <laphayen@gmail.com>
2023-12-21 14:58:54 +0000

    Update thread.c

--- threads/thread.c
+++ threads/thread.c
@@ -430,6 +430,11 @@ init_thread (struct thread *t, const char *name, int priority) {
 	t->tf.rsp = (uint64_t) t + PGSIZE - sizeof (void *);
 	t->priority = priority;
 	t->magic = THREAD_MAGIC;
+
+	/* Priority Inversion */
+	t->init_priority = priority;
+	t->wait_lock = NULL;
+	list_init (&t->donation);
 }
 
 /* Chooses and returns the next thread to be scheduled.  Should
