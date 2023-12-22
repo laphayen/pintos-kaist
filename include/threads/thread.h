c37fdadc368f38df9355fbf68a834e966ea5b150
laphayen <laphayen@gmail.com>
2023-12-22 13:22:28 +0000

    Update thread.h

--- include/threads/thread.h
+++ include/threads/thread.h
@@ -163,4 +163,9 @@ int64_t get_next_tick_to_awake (void);
 void test_max_priority (void);
 bool cmp_priority (const struct list_elem *a, const struct list_elem *b, void *aux UNUSED);
 
+/* Priority Inversion */
+void donate_priority (void);
+void remove_with_lock (struct lock * lock);
+void refresh_priority (void);
+
 #endif /* threads/thread.h */
