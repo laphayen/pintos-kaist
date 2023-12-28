a8b3d4eda26db4c82c1ddd05c8f5c5e4a807c28f
laphayen <laphayen@gmail.com>
2023-12-28 12:22:57 +0000

    Update do.sh

--- do.sh
+++ do.sh
@@ -17,15 +17,15 @@ PRIORITY_PREEMPT="pintos -v -- -q run priority-preempt"
 PRIORITY_SEMA="pintos -v -- -q run priority-sema"
 PRIORITY_CONDVAR="pintos -v -- -q run priority-condvar"
 PRIORITY_DONATE_CHAIN="pintos -v -- -q run priority-donate-chain"
-MLFQS_LOAD_1="pintos -v -- -q run mlfqs-load-1"
-MLFQS_LOAD_60="pintos -v -- -q run mlfqs-load-60"
-MLFQS_LOAD_AVG="pintos -v -- -q run mlfqs-load-avg"
-MLFQS_RECENT_1="pintos -v -- -q run mlfqs-recent-1"
-MLFQS_FAIR_2="pintos -v -- -q run mlfqs-fair-2"
-MLFQS_FAIR_20="pintos -v -- -q run mlfqs-fair-20"
-MLFQS_NICE_2="pintos -v -- -q run mlfqs-nice-2"
-MLFQS_NICE_10="pintos -v -- -q run mlfqs-nice-10"
-MLFQS_BLOCK="pintos -v -- -q run mlfqs-block"
+MLFQS_LOAD_1="pintos -v -k -T 480 -m 20   -- -q  -mlfqs run mlfqs-load-1"
+MLFQS_LOAD_60="pintos -v -k -T 480 -m 20   -- -q  -mlfqs run mlfqs-load-60"
+MLFQS_LOAD_AVG="pintos -v -k -T 480 -m 20   -- -q  -mlfqs run mlfqs-load-avg "
+MLFQS_RECENT_1="pintos -v -k -T 480 -m 20   -- -q  -mlfqs run mlfqs-recent-1"
+MLFQS_FAIR_2="pintos -v -k -T 480 -m 20   -- -q  -mlfqs run mlfqs-fair-2"
+MLFQS_FAIR_20="pintos -v -k -T 480 -m 20   -- -q  -mlfqs run mlfqs-fair-20"
+MLFQS_NICE_2="pintos -v -k -T 480 -m 20   -- -q  -mlfqs run mlfqs-nice-2"
+MLFQS_NICE_10="pintos -v -k -T 480 -m 20   -- -q  -mlfqs run mlfqs-nice-10"
+MLFQS_BLOCK="pintos -v -k -T 480 -m 20   -- -q  -mlfqs run mlfqs-block"
 
 cd threads
 make clean
@@ -33,4 +33,4 @@ make
 cd build
 source ../../activate
 
-$PRIORITY_SEMA
+$MLFQS_LOAD_AVG
