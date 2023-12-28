0d05da6f7c205bb94d38e45975aeae30bebcd987
laphayen <laphayen@gmail.com>
2023-12-28 18:42:52 +0000

    Update fixed_point.h

--- include/threads/fixed_point.h
+++ include/threads/fixed_point.h
@@ -1,3 +1,5 @@
+/* Multi Level Feedback Queue Scheduler */
+
 #define F (1 << 14)
 #define INT_MAX ((1 << 31) - 1)
 #define INT_MIN (-(1 << 31))
@@ -15,46 +17,46 @@ int div_fp (int x, int y);
 int div_mixed (int x, int n);
 
 int int_to_fp (int n) {
-  return n * F;
+    return n * F;
 }
 
 int fp_to_int (int x) {
-  return x / F;
+    return x / F;
 }
 
 int fp_to_int_round (int x) {
-  if (x >= 0) return (x + F / 2) / F;
-  else return (x - F / 2) / F;
+    if (x >= 0) return (x + F / 2) / F;
+    else return (x - F / 2) / F;
 }
 
 int add_fp (int x, int y) {
-  return x + y;
+    return x + y;
 }
 
 int sub_fp (int x, int y) {
-  return x - y;
+    return x - y;
 }
 
 int add_mixed (int x, int n) {
-  return x + n * F;
+    return x + n * F;
 }
 
 int sub_mixed (int x, int n) {
-  return x - n * F;
+    return x - n * F;
 }
 
 int mult_fp (int x, int y) {
-  return ((int64_t) x) * y / F;
+    return ((int64_t) x) * y / F;
 }
 
 int mult_mixed (int x, int n) {
-  return x * n;
+    return x * n;
 }
 
 int div_fp (int x, int y) {
-  return ((int64_t) x) * F / y;
+    return ((int64_t) x) * F / y;
 }
 
 int div_mixed (int x, int n) {
-  return x / n;
+    return x / n;
 }
