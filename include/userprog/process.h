5e09714a323148d7e98e107ffc14794faefc4e77
laphayen <laphayen@gmail.com>
2024-01-01 12:00:23 +0000

    Update process.h

--- include/userprog/process.h
+++ include/userprog/process.h
@@ -11,6 +11,6 @@ void process_exit (void);
 void process_activate (struct thread *next);
 
 /* Argument Passing */
-void argument_stack (char **parse, int count, void **esp);
+void argument_stack (char **parse, int count, void **rsp);
 
 #endif /* userprog/process.h */
