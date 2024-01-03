25770eebc932359f5c0047f8c26d36d2dd63b1b4
laphayen <laphayen@gmail.com>
2024-01-03 19:41:51 +0000

    Update synch.c

--- threads/synch.c
+++ threads/synch.c
@@ -112,7 +112,7 @@ sema_up (struct semaphore *sema) {
 	old_level = intr_disable ();
 	/* Priority Scheduling and Synchronization */
 	if (!list_empty (&sema->waiters)) {
-		list_sort(&sema->waiters, &cmp_priority, NULL);
+		list_sort (&sema->waiters, cmp_priority, NULL);
 		thread_unblock (list_entry (list_pop_front (&sema->waiters),
 					struct thread, elem));
  	}
