5644cf787516a8ea3cbadd07084718da6cd3f3e3
laphayen <laphayen@gmail.com>
2024-01-06 12:25:47 +0000

    Update syscall.h

--- include/userprog/syscall.h
+++ include/userprog/syscall.h
@@ -6,4 +6,8 @@ void syscall_init (void);
 /* User Memory Access */
 void check_address (void *addr);
 
+/* System Call */
+void halt (void);
+void exit (int status);
+
 #endif /* userprog/syscall.h */
