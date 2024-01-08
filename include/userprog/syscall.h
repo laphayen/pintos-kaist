f5df4d6c28b5097941094007c263d3560899adf6
laphayen <laphayen@gmail.com>
2024-01-08 12:40:35 +0000

    Update syscall.h

--- include/userprog/syscall.h
+++ include/userprog/syscall.h
@@ -14,5 +14,13 @@ int exec (const char *file);
 int wait (tid_t tid_t);
 bool create (const char *file, unsigned initial_size);
 bool remove (const char *file);
+int open (const char *file);
+int filesize (int fd);
+int read (int fd, void *buffer, unsigned size);
+int write (int fd, const void *buffer, unsigned size);
+void seek (int fd, unsigned position);
+unsigned tell (int fd);
+void close (int fd);
+int dup2 (int oldfd, int newfd);
 
 #endif /* userprog/syscall.h */
