fd90e3ef0e94731e3f1dd5ef9c4d69e48fef9115
laphayen <laphayen@gmail.com>
2024-01-11 09:09:31 +0000

    Update thread.c

--- threads/thread.c
+++ threads/thread.c
@@ -490,6 +490,10 @@ init_thread (struct thread *t, const char *name, int priority) {
 	/* Multi Level Feedback Queue Scheduler */
 	t->nice = NICE_DEFAULT;
 	t->recent_cpu = LOAD_AVG_DEFAULT;
+
+	/* Hierarchical Process Structure */
+	// sema_init (&t->exit_sema, 0);
+	// sema_init (&t->wait_sema, 0);
 }
 
 /* Chooses and returns the next thread to be scheduled.  Should
