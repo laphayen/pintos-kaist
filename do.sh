2c581f6e51d4548182390cd01face7afac09a3cb
laphayen <laphayen@gmail.com>
2024-01-31 09:36:57 +0000

    Update syscall.c

--- do.sh
+++ do.sh
@@ -32,4 +32,4 @@ make clean
 make
 cd build
 source ../../activate
-pintos -v -k -T 60 -m 20   --fs-disk=10 -p tests/userprog/multi-child-fd:multi-child-fd -p ../../tests/userprog/sample.txt:sample.txt -p tests/userprog/child-close:child-close -- -q   -f run multi-child-fd
+pintos -v -k -T 60 -m 20   --fs-disk=10 -p tests/userprog/rox-child:rox-child -p tests/userprog/child-rox:child-rox -- -q   -f run rox-child
