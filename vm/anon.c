c11e304a5964555e4bd9ddd6d28d306206e3fd2b
Ubuntu <ubuntu@ip-172-31-39-164.ap-northeast-2.compute.internal>
2024-02-13 09:06:04 +0000

    Update anon.c

--- vm/anon.c
+++ vm/anon.c
@@ -29,13 +29,14 @@ bool
 anon_initializer (struct page *page, enum vm_type type, void *kva) {
 	/* Anonymous Page */
 	struct uninit_page *uninit = &page->uninit;
+
 	memset (uninit, 0, sizeof (struct uninit_page));
 
 	/* Set up the handler */
 	page->operations = &anon_ops;
 
-	struct anon_page *anon_page = &page->anon;
-	anon_page->swap_info = -1;
+	struct anon_page *anon = &page->anon;
+	anon->swap_info = -1;
 	
 	return true;
 }
