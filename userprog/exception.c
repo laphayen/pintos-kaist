4006c1279ed8820fa4fd07bcf2d2ad9c0c907e60
laphayen <laphayen@gmail.com>
2024-03-28 07:43:54 +0000

    Refactor: Call exit(-1) directly after invoking vm_try_handle_fault() to terminate page fault handling at that point

--- userprog/exception.c
+++ userprog/exception.c
@@ -145,7 +145,7 @@ page_fault (struct intr_frame *f) {
 	if (vm_try_handle_fault (f, fault_addr, user, write, not_present))
 		return;
 #endif
-
+	exit(-1);
 	/* Count page faults. */
 	page_fault_cnt++;
 
@@ -156,7 +156,6 @@ page_fault (struct intr_frame *f) {
 			write ? "writing" : "reading",
 			user ? "user" : "kernel");
 	/* File Descriptor */
-	// kill (f);
-	exit(-1);
+	kill (f);	
 }
 
