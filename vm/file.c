f3973b05f74bb7b35b1591c18b639a6511e0620b
laphayen <laphayen@gmail.com>
2024-04-16 14:58:35 +0000

    Feat: Implement file_backed_swap_in and file_backed_swap_out

--- vm/file.c
+++ vm/file.c
@@ -49,12 +49,32 @@ file_backed_initializer (struct page *page, enum vm_type type, void *kva) {
 static bool
 file_backed_swap_in (struct page *page, void *kva) {
 	struct file_page *file_page UNUSED = &page->file;
+	
+	return lazy_load_segment_file (page, file_page);
 }
 
 /* Swap out the page by writeback contents to the file. */
 static bool
 file_backed_swap_out (struct page *page) {
 	struct file_page *file_page UNUSED = &page->file;
+	struct thread *curr = thread_current ();
+
+	if (page == NULL) {
+		return false;
+	}
+
+	struct load_aux *load_aux = (struct load_aux*)page->uninit.aux;
+	struct file *file_obj = load_aux->file;
+
+	if (pml4_is_dirty (thread_current ()->pml4, page->va)) {
+		file_write_at (file_obj, page->va, load_aux->read_bytes, load_aux->ofs);
+
+		pml4_set_dirty (thread_current ()->pml4, page->va, false);
+	}
+
+	pml4_clear_page (thread_current ()->pml4, page->va);
+
+	return true;
 }
 
 /* Destory the file backed page. PAGE will be freed by the caller. */
@@ -82,11 +102,14 @@ do_mmap (void *addr, size_t length, int writable,
 		return NULL;
 	}
 
-	struct page *page;
 	void *init_addr = addr;
 
 	size_t read_bytes = length > file_length(file) ? file_length (file) : length;
 	size_t zero_bytes = PGSIZE - read_bytes % PGSIZE;
+
+	ASSERT (pg_ofs (addr) == 0);
+	ASSERT (offset % PGSIZE == 0);
+	ASSERT ((read_bytes + zero_bytes) % PGSIZE == 0);
 
 	while (read_bytes > 0 || zero_bytes > 0) {
 		size_t page_read_bytes = read_bytes < PGSIZE ? read_bytes : PGSIZE;
@@ -96,14 +119,16 @@ do_mmap (void *addr, size_t length, int writable,
 
 		aux->file = file_obj;
 		aux->ofs = offset;
-		aux->read_bytes = read_bytes;
-		aux->zero_bytes = zero_bytes;
+		aux->read_bytes = page_read_bytes;
+		aux->zero_bytes = page_zero_bytes;
 
 		struct page *p = spt_find_page(&thread_current()->spt, init_addr);
 
 		if (!vm_alloc_page_with_initializer (VM_FILE, addr, writable, lazy_load_segment_file, aux)) {
 			return NULL;
 		}
+
+		struct page *page = spt_find_page (&thread_current ()->spt, init_addr);
 
 		read_bytes -= page_read_bytes;
 		zero_bytes -= page_zero_bytes;
